0c93c2624ce32088f3900a663bd6bbaac1517eb9
Nitro <xiaoren8688@yahoo.co.jp>
2020-07-08 16:23:32 +0900

    update

--- web.go
+++ web.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"database/sql"
+	_ "fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func connect() {
+	var er error
+	db, er := sql.Open("mysql", "root:123456@tcp(localhost:3306)/world?charset=utf8")
+
+	if er != nil {
+		panic("db connect error")
+	}
+
+	if e := db.Ping(); e != nil {
+		panic("ping error")
+	}
+}
+
 func main() {
 
 }
